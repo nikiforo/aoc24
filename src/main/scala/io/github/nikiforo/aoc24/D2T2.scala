a6f1851b2a28ddbde1fda52df75903156190881a
nikiforo <anikiforov@delimobil.ru>
2024-12-02 08:39:27 +0300

    D2

--- src/main/scala/io/github/nikiforo/aoc24/D2T2.scala
+++ src/main/scala/io/github/nikiforo/aoc24/D2T2.scala
@@ -2,7 +2,7 @@ package io.github.nikiforo.aoc24
 
 object D2T2 {
 
-  def main(args: Array[String]): Unit = {
+  def solve(args: Array[String]): Unit = {
     val lines = aocLines("2")
     println(compute(lines))
   }
@@ -10,7 +10,7 @@ object D2T2 {
   def compute(lines: List[String]): Long =
     lines.count { line =>
       val ints = line.split(" ").toList.map(i => i.toInt)
-      ints.indices.toList.exists { ind =>
+      ints.indices.exists { ind =>
         val dropped = ints.take(ind) ::: ints.drop(ind + 1)
         val diffs = dropped.zip(dropped.tail).map { case (i, j) => i - j }
         diffs.forall(i => 0 < i && i <= 3) || diffs.forall(i => -3 <= i && i < 0)
